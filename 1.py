1fd5754f36c60be98c5bb62f920f9186ce985e4d
Jaxongir2004 <hoshimovulugbek8@gmail.com>
2025-03-14 20:14:34 +0500

    Bot kodi yangilandi

--- 1.py
+++ 1.py
@@ -38,3 +38,14 @@ async def main():
 
 if __name__ == "__main__":
     asyncio.run(main())
+    from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def webhook():
+    return "Bot ishlayapti!", 200
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
+
